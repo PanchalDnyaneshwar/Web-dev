6882dfe588ce89d190b18ab615f343b2203d14c0
panchal Dnyaneshwar <panchaldnyaneshwar.m@gmail.com>
2024-04-14 13:59:10 +0530

    add new  file

--- Back-end/ 3.2 HTTP Request/index.js
+++ Back-end/ 3.2 HTTP Request/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 const app = express();
+const port = 3000;
 
-app.get("./", (req , res) =>{
-    console.log(req);
-})
+app.get("./", (req, res) => {
+  res.send("<h1>Hello</h1>");
+});
 
-app.listen(3000, () => {
-    console.log("server run on port 3000");
-})
+app.listen(port, () => {
+  console.log(`server run on port ${port}`);
+});
+            
